68dc19e967fe44dbba140c40eb2648a9e6fe9f8f
oteikwu1 <oteikwuon@gmail.com>
2026-05-27 06:44:13 +0100

    implement all the css styles

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
-const searchInput = document.getElementById('search-input');
-const recipeBtn = document.querySelector('.recipe-btn');
-const displayContainer = document.querySelector('.display-container');
+const numberInput = document.getElementById('number-input');
+const checkBtn = document.getElementById('check-btn');
+const displayResult = document.querySelector('.display-result');
 
-
-function  searchRecipe() {
-    const query = searchInput.value;
-searchInput.value = '';
-   const url = `https://www.themealdb.com/api/json/v1/1/random.php?s=${query}`;
-console.log(url)
-   displayContainer.innerHTML = "<p>Searching for recipe.....</P>";
-
-   fetch(url)
-   .then(response => {
-    if(!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    return response.json()
-   })
-   .then(data => {
-    displayContainer.innerHTML = "";
-    if (data.meal) {
-        renderMeals(data.meal)
-    } else{
-       displayContainer.innerHTML = "<p>no meal found try another keywords</P>";  
-    }
-   })
-   .catch(error => {
-    displayContainer.innerHTML = `<p>Error: ${error.message}</p>`
-   })
-};
-
-function renderMeals() {
-    const mealDiv = document.createElement('newDiv')
+function checkNumber(num){
+    let inputValue = numberInput.value.trim();
+    numberInput.value = '';
     
-   
+    if(isNaN(num) || num <= 1){
+        displayResult.textContent = `Please is not a valid number`;
+        displayResult.style.color = 'red';
+        return
+    }
 }
 
-recipeBtn.addEventListener('click', searchRecipe)
-//  searchRecipe() 
+let isPrimeNumber = true;
+
+for (let i = 2; i < num.length; i++) {
+
+}
